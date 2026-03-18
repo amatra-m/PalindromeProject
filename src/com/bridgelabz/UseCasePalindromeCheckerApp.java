68b88d34449c21f4cb74e3d5ff72786aa8f3a68a
amatra-m <amatramenoon7@gmail.com>
2026-03-18 09:41:23 +0530

    UC1

--- src/com/bridgelabz/UseCasePalindromeCheckerApp.java
+++ src/com/bridgelabz/UseCasePalindromeCheckerApp.java
@@ -1,7 +1,27 @@
 package com.bridgelabz;
 
+import java.util.Scanner;
+
 public class UseCasePalindromeCheckerApp {
     public static void main(String[] args) {
-        System.out.println("Welcome Palindrome");
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a word: ");
+        String original = sc.nextLine();
+
+        String reversed = "";
+
+        // reverse the string
+        for (int i = original.length() - 1; i >= 0; i--) {
+            reversed = reversed + original.charAt(i);
+        }
+
+        // check palindrome
+        if (original.equals(reversed)) {
+            System.out.println("It is a Palindrome");
+        } else {
+            System.out.println("Not a Palindrome");
+        }
     }
 }
